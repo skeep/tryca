0c8ae30b9c0bd8272bc15c4c5565cd25c8b6870c
Suman Paul <suman.paul@optus.com.au>
2021-08-08 21:15:53 +0530

    chnge nav file

--- nav.js
+++ nav.js
@@ -3,38 +3,38 @@ var navItems = [{
     link: ''
 }, {
     label: 'Eligibility',
-    link: '2.html'
+    link: '/2.html'
 },{
     label: 'eKYC',
-    link: '3.html'
+    link: '/3.html'
 },{
     label: 'Establishment Details',
-    link: '4.html'
+    link: '/4.html'
 },{
     label: 'Personal Details',
-    link: '5.html'
+    link: '/5.html'
 },{
     label: 'Charges and nomination',
-    link: '6.html'
+    link: '/6.html'
 },{
     label: 'Common Declaration',
-    link: '6.html'
+    link: '/6.html'
 },{
     label: 'Video KYC',
-    link: '6.html'
+    link: '/6.html'
 },{
     label: 'Digital fuding',
-    link: '6.html'
+    link: '/6.html'
 },{
     label: 'Thank you',
-    link: '6.html'
+    link: '/6.html'
 }];
 
 var navRender = `
 <ul class="nav flex-column">
 ${navItems.map(item => `
 <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="/${item.link}">${item.label}</a>
+    <a class="nav-link active" aria-current="page" href="${item.link}">${item.label}</a>
 </li>
 `).join('')}
 </ul>
